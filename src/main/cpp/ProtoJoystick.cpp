8dcd17b67072ce590a4ec689ad316bd671012f2f
Blue <shamsrami722@gmail.com>
2019-01-26 10:25:43 -0500

    Added buttons to protojoystick

--- src/main/cpp/ProtoJoystick.cpp
+++ src/main/cpp/ProtoJoystick.cpp
@@ -8,8 +8,113 @@
 #include "ProtoJoystick.h"
 
 ProtoJoystick::ProtoJoystick(unsigned int port) 
-: Joystick(port)
-{}
+: Joystick(port) {
+f_A = f_B = f_X = f_Y = f_LB = f_RB = f_Back = f_Start = f_L3 = f_R3 = f_LT = f_RT = false;
+}
+
+
+bool ProtoJoystick::Button(int btn) {
+	if ((btn & kButtonA) && !Button(kButtonA)) {
+		return false;
+	} else if ((btn & kButtonB) && !Button(kButtonB)) {
+		return false;
+	} else if ((btn & kButtonX) && !Button(kButtonX)) {
+		return false;
+	} else if ((btn & kButtonY) && !Button(kButtonY)) {
+		return false;
+	} else if ((btn & kButtonLB) && !Button(kButtonLB)) {
+		return false;
+	} else if ((btn & kButtonRB) && !Button(kButtonRB)) {
+		return false;
+	} else if ((btn & kButtonLT) && !Button(kButtonLT)) {
+		return false;
+	} else if ((btn & kButtonRT) && !Button(kButtonRT)) {
+		return false;
+	}
+
+	return true;
+}
+
+bool ProtoJoystick::ButtonPressed(kButton btn) {
+	switch (btn) {
+	case kButtonA:
+		if (Button(btn)) {
+			return (f_A) ? false : f_A = true;
+		} else {
+			return f_A = false;
+		}
+	case kButtonB:
+		if (Button(btn)) {
+			return (f_B) ? false : f_B = true;
+		} else {
+			return f_B = false;
+		}
+	case kButtonX:
+		if (Button(btn)) {
+			return (f_X) ? false : f_X = true;
+		} else {
+			return f_X = false;
+		}
+	case kButtonY:
+		if (Button(btn)) {
+			return (f_Y) ? false : f_Y = true;
+		} else {
+			return f_Y = false;
+		}
+	case kButtonLB:
+		if (Button(btn)) {
+			return (f_LB) ? false : f_LB = true;
+		} else {
+			return f_LB = false;
+		}
+	case kButtonRB:
+		if (Button(btn)) {
+			return (f_RB) ? false : f_RB = true;
+		} else {
+			return f_RB = false;
+		}
+	case kButtonBack:
+		if (Button(btn)) {
+			return (f_Back) ? false : f_Back = true;
+		} else {
+			return f_Back = false;
+		}
+	case kButtonStart:
+		if (Button(btn)) {
+			return (f_Start) ? false : f_Start = true;
+		} else {
+			return f_Start = false;
+		}
+	case kButtonL3:
+		if (Button(btn)) {
+			return (f_L3) ? false : f_L3 = true;
+		} else {
+			return f_L3 = false;
+		}
+	case kButtonR3:
+		if (Button(btn)) {
+			return (f_R3) ? false : f_R3 = true;
+		} else {
+			return f_R3 = false;
+		}
+	case kButtonLT:
+		if (Button(btn)) {
+			return (f_LT) ? false : f_LT = true;
+		} else {
+			return f_LT = false;
+		}
+	case kButtonRT:
+		if (Button(btn)) {
+			return (f_RT) ? false : f_RT = true;
+		} else {
+			return f_RT = false;
+		}
+	default:
+		return false;
+	}
+}
+
+
 
 float ProtoJoystick::Axis(kAxis axis) {
  switch(axis) {
--- src/main/include/ProtoJoystick.h
+++ src/main/include/ProtoJoystick.h
@@ -12,6 +12,28 @@
 
 class ProtoJoystick : public frc::Joystick {
  public:
+
+ typedef enum {
+		kButtonA = 1 << 0,
+		kButtonB = 1 << 1,
+		kButtonX = 1 << 2,
+		kButtonY = 1 << 3,
+		kButtonLB = 1 << 4,
+		kButtonRB = 1 << 5,
+		kButtonBack = 1 << 6,
+		kButtonStart = 1 << 7,
+		kButtonL3 = 1 << 8,
+		kButtonR3 = 1 << 9,
+
+		/**
+		 * 	Next Two:
+		 * 		Using triggers as buttons (good for shooting)
+		 * 		These buttons are on if the triggers are pressed more than 40%
+		 */
+		kButtonLT = 1 << 10,
+		kButtonRT = 1 << 11
+	} kButton;
+
 typedef enum {
 	kAxisLX = 1 << 0,
 	kAxisLY = 1 << 1,
@@ -24,8 +46,36 @@ typedef enum {
 
   ProtoJoystick(unsigned int port);
 
-  float Axis(kAxis axis);
 
+  bool Button(int btns);
+  float Axis(kAxis axis);
+  bool ButtonPressed(kButton btn);
+
+	bool ButtonA() { return Button(kButtonA); }
+	bool ButtonB() { return Button(kButtonB); }
+	bool ButtonX() { return Button(kButtonX); }
+	bool ButtonY() { return Button(kButtonY); }
+	bool ButtonLB() { return Button(kButtonLB); }
+	bool ButtonRB() { return Button(kButtonRB); }
+	bool ButtonBack() { return Button(kButtonBack); }
+	bool ButtonStart() { return Button(kButtonStart); }
+	bool ButtonL3() { return Button(kButtonL3); }
+	bool ButtonR3() { return Button(kButtonR3); }
+	bool ButtonLT() { return Button(kButtonLT); }
+	bool ButtonRT() { return Button(kButtonRT); }
+
+  bool ButtonPressedA() { return ButtonPressed(kButtonA); }
+	bool ButtonPressedB() { return ButtonPressed(kButtonB); }
+	bool ButtonPressedX() { return ButtonPressed(kButtonX); }
+	bool ButtonPressedY() { return ButtonPressed(kButtonY); }
+	bool ButtonPressedLB() { return ButtonPressed(kButtonLB); }
+	bool ButtonPressedRB() { return ButtonPressed(kButtonRB); }
+	bool ButtonPressedStart() { return ButtonPressed(kButtonStart); }
+	bool ButtonPressedBack() { return ButtonPressed(kButtonBack); }
+	bool ButtonPressedL3() { return ButtonPressed(kButtonL3); }
+	bool ButtonPressedR3() { return ButtonPressed(kButtonR3); }
+	bool ButtonPressedLT() { return ButtonPressed(kButtonLT); }
+	bool ButtonPressedRT() { return ButtonPressed(kButtonRT); }
 
   float AxisLX() { return Axis(kAxisLX); }
 	float AxisLY() { return Axis(kAxisLY); }
@@ -33,4 +83,7 @@ typedef enum {
 	float AxisRY() { return Axis(kAxisRY); }
 	float AxisLT() { return Axis(kAxisLT); }
 	float AxisRT() { return Axis(kAxisRT); }
+
+  private:
+  bool f_A, f_B, f_X, f_Y, f_LB, f_RB, f_Back, f_Start, f_L3, f_R3, f_LT, f_RT;
 };
